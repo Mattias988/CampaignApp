be4083655c25d5cd5fdc845e413dd6b00d683440
MKorsakowsky <maciek.korsakowski@gmail.com>
2024-07-07 16:04:30 +0200

    add campaign entity

---
+++ src/main/java/com/campaign/Entity/Campaign.java
@@ -0,0 +1,46 @@
+package com.campaign.Entity;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "")
+public class Campaign {
+
+    @Id
+    @Generated
+    private Long campaignId;
+
+    @NotBlank(message = "Campaign name cannot be blank!")
+    private String campaignName;
+
+    @ElementCollection
+    @NotNull
+    @NotBlank
+    private List<String> keywords;
+
+    @NotNull(message = "Bid amount must be higher than zero!")
+    private Long bidAmount;
+
+    @NotNull
+    private Long campaignFund;
+
+    @NotEmpty
+    private Boolean status;
+
+    @NotBlank
+    private String town;
+
+    @NotNull
+    private Long radius;
+}
