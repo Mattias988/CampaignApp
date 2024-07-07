9e16998c493c3961bc12d889d6d69f6d258a0965
Mattias988 <maciek.korsakowski@gmail.com>
2024-07-07 17:27:06 +0200

    add annotations

--- src/main/java/com/campaign/DTOs/CampaignDTO.java
+++ src/main/java/com/campaign/DTOs/CampaignDTO.java
@@ -3,23 +3,32 @@ package com.campaign.DTOs;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter
 @Setter
 public class CampaignDTO {
 
+    @NotBlank
     private String CampaignName;
 
+    @NotNull
     private Long bidAmount;
 
+    @NotBlank
     private List<String> keywords;
 
+    @NotNull
     private Long campaignFund;
 
+    @NotBlank
     private Boolean status;
 
+    @NotBlank
     private String Town;
 
+    @NotNull
     private Long radius;
 }
