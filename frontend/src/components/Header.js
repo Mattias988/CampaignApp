e0e22776e2edc6711028586d51569320f264dd78
Mattias988 <maciek.korsakowski@gmail.com>
2024-07-08 19:30:04 +0200

    add header

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+const Header = ({ onAddCampaign, searchQuery, setSearchQuery }) => {
+  return (
+    <header className="bg-gradient-to-r from-blue-300 to-blue-800 p-4 text-white flex flex-col md:flex-row justify-between items-center shadow-xl">
+        <div className="flex flex-col md:flex-row items-center sm:w-auto md:w-72 shadow-lg">
+            <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+        </div>
+        <div className=''>
+            <img src="/logo.png" alt='Campaigns' width="150px" height="150px" className=""/>
+        </div>
+        <button 
+            onClick={onAddCampaign} 
+            className="bg-white text-blue-600 px-4 py-2 rounded mt-4 md:mt-0 shadow-lg"
+        >
+            Add Campaign
+        </button>
+    </header>
+  );
+};
+
+export default Header;
