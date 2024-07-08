5859c17b75e8c9a591e74241a5fb8449dc4567a9
Mattias988 <maciek.korsakowski@gmail.com>
2024-07-08 19:27:20 +0200

    add exception handler and validation

--- src/main/java/com/campaign/Controller/CampaignController.java
+++ src/main/java/com/campaign/Controller/CampaignController.java
@@ -2,27 +2,28 @@ package com.campaign.Controller;
 
 import com.campaign.DTOs.CampaignDTO;
 import com.campaign.Entity.Campaign;
-import com.campaign.Repository.CampaignRepository;
 import com.campaign.Service.CampaignService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/campaign")
 public class CampaignController {
 
     @Autowired
-    private CampaignRepository campaignRepository;
-
-    @Autowired
     private CampaignService campaignService;
 
     @PostMapping
-    public ResponseEntity<Campaign> addCampaign(@RequestBody Campaign campaign) {
+    public ResponseEntity<Campaign> addCampaign(@Valid @RequestBody Campaign campaign) {
         Campaign newCampaign = campaignService.addCampaign(campaign);
         return new ResponseEntity<>(newCampaign, HttpStatus.CREATED);
     }
@@ -46,8 +47,21 @@ public class CampaignController {
     }
 
     @PutMapping("/{campaignId}")
-    public ResponseEntity<Campaign> updateCampaign(@PathVariable Long campaignId, @RequestBody CampaignDTO campaignDTO) {
+    public ResponseEntity<Campaign> updateCampaign(@PathVariable Long campaignId, @Valid @RequestBody CampaignDTO campaignDTO) {
         Campaign campaign = campaignService.updateCampaign(campaignId, campaignDTO);
         return new ResponseEntity<>(campaign, HttpStatus.OK);
+    }
+
+    //Exception handler for easier error management and easier operation on the frontend
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.badRequest().body(errors);
     }
 }
